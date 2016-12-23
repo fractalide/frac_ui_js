c51cda6918b999786d7a33da3f53193f29ba198d
Stewart Mackenzie <setori88@gmail.com>
2016-12-23 17:12:34 +0800

    Problem: ip_* -> msg_* rename not supported
    Solution: support it

--- nodes/edit/default.nix
+++ nodes/edit/default.nix
@@ -3,13 +3,13 @@
 subgraph {
   src = ./.;
   flowscript = with nodes; with edges; ''
-   input => input in_dispatch(${ip_dispatcher}) output -> input out_dispatch(${ip_dispatcher}) output => output
+   input => input in_dispatch(${msg_dispatcher}) output -> input out_dispatch(${msg_dispatcher}) output => output
 
    model(${app_model}) output -> input view(${ui_js_nodes.edit_view}) output -> input out_dispatch()
 
    '${generic_text}:(text="")' -> acc model()
 
-   in_dispatch() output[create] -> input create_clone(${ip_clone})
+   in_dispatch() output[create] -> input create_clone(${msg_clone})
    create_clone() clone[1] -> input view()
    create_clone() clone[2] -> input model()
 
--- nodes/edit/view/default.nix
+++ nodes/edit/view/default.nix
@@ -3,14 +3,14 @@
 subgraph {
   src = ./.;
   flowscript = with nodes; with edges; ''
-   input => input in_dispatch(${ip_dispatcher}) output -> input out_dispatch(${ip_dispatcher}) output => output
+   input => input in_dispatch(${msg_dispatcher}) output -> input out_dispatch(${msg_dispatcher}) output => output
 
    ph(${ui_js_nodes.placeholder}) output -> input out_dispatch()
 
    text(${ui_js_nodes.tag}) output -> places[1] ph()
    input(${ui_js_nodes.tag}) output -> places[2] ph()
 
-   text() output[dblclick] -> input disp_input(${ip_action}) output -> input input()
+   text() output[dblclick] -> input disp_input(${msg_action}) output -> input input()
    '${generic_text}:(text="display")' -> option disp_input()
 
    input() output[keyup] -> input key_filter(${ui_js_nodes.edit_keyfilter})
@@ -32,7 +32,7 @@ subgraph {
    create() text -> input text()
    create() input -> input input()
 
-   in_dispatch() output[delete] -> input clone(${ip_clone})
+   in_dispatch() output[delete] -> input clone(${msg_clone})
    clone() clone[1] -> input text()
    clone() clone[2] -> input input()
    clone() clone[3] -> input ph()
